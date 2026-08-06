ab868d6146b5902fb67503882a7bfe31748e3848
Ishara Madusanka <128658364+Madusankadev@users.noreply.github.com>
2026-08-06 23:31:43 +0530

    Mouse event handle => left click, right click, wheel scroll, left & right double click

--- Lesson_10_Mouse_Clicks_and_Events.py
+++ Lesson_10_Mouse_Clicks_and_Events.py
@@ -1,16 +1,73 @@
+# how to launch web cam more faster
+
 import cv2
 
 print(cv2.__version__)
 
-cam = cv2.VideoCapture(0)
+width = 1280
+height = 720
+evt=0
+
+def mouseClick(event, xPOs, yPos, flag, param):
+    global evt
+    global pos
+    if event == cv2.EVENT_LBUTTONDOWN:
+        evt=event
+        pos=(xPOs, yPos)
+    if event==cv2.EVENT_LBUTTONUP:
+        evt=event
+        pos=(xPOs,yPos)
+    if event==cv2.EVENT_RBUTTONDOWN:
+        evt=event
+        pos=(xPOs,yPos)
+    if event==cv2.EVENT_RBUTTONUP:
+        evt=event
+        pos=(xPOs,yPos)
+
+    if event==cv2.EVENT_MOUSEWHEEL:
+        print(f"Event: {event}")
+        print(f"POSITIONS: x:{xPOs}, y:{yPos}")
+
+    evt=event
+    pos=(xPOs, yPos)
+
+cam = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+cam.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+cam.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+cam.set(cv2.CAP_PROP_FPS, 30)
+cam.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG')) # setup cam codak
+
+cv2.namedWindow('CAM')
+cv2.setMouseCallback('CAM', mouseClick)
 
 while True:
     _ignore, frame = cam.read()
 
-    grayFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    if evt==cv2.EVENT_LBUTTONDOWN:
+        cv2.circle(frame, pos, 25, (255, 0, 0), 2)
+        cv2.putText(frame, "Mouse Left Down",pos, cv2.FONT_HERSHEY_SIMPLEX, 2, (0,255,0),2)
 
-    cv2.imshow('My Web CAM', grayFrame)
-    cv2.moveWindow('My Web CAM', 0, 0)
+    if evt==cv2.EVENT_LBUTTONUP:
+        cv2.circle(frame, pos, 25, (255, 0, 0), 2)
+        cv2.putText(frame, "Mouse Left Up", pos, cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255,0), 2)
+
+    if evt==cv2.EVENT_RBUTTONDOWN:
+        cv2.putText(frame, "Mouse Right Down", pos, cv2.FONT_HERSHEY_SIMPLEX, 2, (0,255,0),2)
+        cv2.circle(frame, pos, 25, (255,0,0), 2)
+
+    if evt==cv2.EVENT_RBUTTONUP:
+        cv2.putText(frame, "Mouse Right Up", pos, cv2.FONT_HERSHEY_SIMPLEX, 2, (0,255,0),2)
+        cv2.circle(frame, pos, 25, (255,0,0),2)
+
+    if evt==cv2.EVENT_LBUTTONDBLCLK:
+        print("Mouse left double click")
+        cv2.putText(frame, "Left Button Double Clicked", pos, cv2.FONT_HERSHEY_SIMPLEX, 2, (0,255,0),2)
+
+    if evt==cv2.EVENT_RBUTTONDBLCLK:
+        print("Right button double clicked")
+
+    cv2.imshow('CAM', frame)
+    cv2.moveWindow('CAM', 0, 0)
 
     if cv2.waitKey(1) & 0xff == ord('q'):
         break
